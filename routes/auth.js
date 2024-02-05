766f53e35dc0a80d26aff996060d00a18374e9fd
Gourab Singha <gaurabsingha16@gmail.com>
2024-02-05 17:33:40 +0530

    third

--- routes/auth.js
+++ routes/auth.js
@@ -5,7 +5,7 @@ router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ email: req.body.email })
         if(!user) {
-            res.status(400).json({ message: "user not found", status: false })
+            res.status(400).json({ message: " not found", status: false })
         }
         else {
             const validatePassword = req.body.password == user.password
